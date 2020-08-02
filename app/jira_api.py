3713a2b86ee0fd9024049025a463c68ac34868df
dmitriy <daenwer@gmail.com>
2020-08-02 23:10:05 +0300

    last option

--- app/jira_api.py
+++ app/jira_api.py
@@ -6,13 +6,6 @@ def create_new_task_jira(description):
     jira_options = {'server': 'https://daenver.atlassian.net/'}
     jira = JIRA(basic_auth=(JIRA_USERNAME, JIRA_TOKEN), options=jira_options)
 
-    # all_issues = jira.search_issues(
-    #     "project=TI",
-    #     startAt=0,
-    #     maxResults=1000,
-    #     validate_query=True,
-    # )
-
     new_task = jira.create_issue(
         fields={
             'project': {'key': 'TI'},
@@ -25,8 +18,3 @@ def create_new_task_jira(description):
     )
 
     return new_task
-
-
-# if __name__ == '__main__':
-#     name = 'New task for test'
-#     create_new_task(description=name)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from django.views import View
 from django.http import JsonResponse
-from rest_framework.response import Response
-from rest_framework import status
+
 from app.slack_api import *
 
 
